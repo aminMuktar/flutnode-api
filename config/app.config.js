80c423ab1984f630d726955a6b6daf20c8ec4ad8
aminM <60422727+aminMuktar@users.noreply.github.com>
2023-10-05 11:59:05 +0300

    fixed security issue

--- config/app.config.js
+++ config/app.config.js
@@ -1,5 +1,5 @@
 const MONGO_DB_CONFIG = {
-  DB: "mongodb+srv://amin:5AOZGPvL5YenuLWP@cluster0.r0ofpnk.mongodb.net/flutnode?retryWrites=true&w=majority"  
+  DB: env.MONGO_DB_URI  
 };
 
 module.exports = {
